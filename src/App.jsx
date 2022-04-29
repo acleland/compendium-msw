f669774b13c9faf818d13c4cb05cc15a596134c4
Andrea Cleland <andreaclelan@gmail.com>
2022-04-29 08:55:43 -0700

    got cat pics to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { fetchCatPics } from './services/fetch';
+import { useState, useEffect } from 'react';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [catPics, setCatPics] = useState([]);
+  useEffect(() => {
+    const getCatPics = async () => {
+      const data = await fetchCatPics();
+      setCatPics(data);
+    };
+    getCatPics();
+  }, []);
+
+  return (
+    <>
+      <h1>The Cat Pic Compendium</h1>
+
+      <div className="list-container">
+        {catPics.map((catPic) => (
+          <img key={catPic.id} src={`${catPic.url}`} alt={`cat image`} />
+        ))}
+      </div>
+    </>
+  );
 }
---
+++ src/services/fetch.js
@@ -0,0 +1,17 @@
+export async function fetchCharacters() {
+  const resp = await fetch('https://officeapi.dev/api/characters/');
+  console.log('resp', resp);
+  const data = await resp.json();
+  console.log('json data', data);
+  return data;
+}
+
+export async function fetchCatPics() {
+  const resp = await fetch(
+    'https://api.thecatapi.com/v1/images/search?limit=10'
+  );
+  console.log('resp', resp);
+  const data = await resp.json();
+  console.log('json data', data);
+  return data;
+}
